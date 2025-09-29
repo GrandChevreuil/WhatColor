03c5681eaa184e4485e455b911b43e11266b012e
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-09-29 09:23:39 +0200

    update - welcome screen updated to use the view model

--- app/src/main/java/fr/eseo/ld/mm/whatcolor/ui/screens/WelcomeScreenComposables.kt
+++ app/src/main/java/fr/eseo/ld/mm/whatcolor/ui/screens/WelcomeScreenComposables.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -15,6 +17,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fr.eseo.ld.mm.whatcolor.R
+import fr.eseo.ld.mm.whatcolor.ui.state.GameUiState
+import fr.eseo.ld.mm.whatcolor.ui.viewmodels.GameViewModel
 
 @Composable
 fun WelcomeTitle() {
@@ -51,15 +55,15 @@ fun WelcomeButton(
 }
 
 @Composable
-fun WelcomeScores() {
+fun WelcomeScores(gameUiState: GameUiState) {
     Text(
-        text = stringResource(R.string.last_score, 0),
+        text = stringResource(R.string.last_score, gameUiState.lastScore),
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
         modifier = Modifier.padding(8.dp)
     )
     Text(
-        text = stringResource(R.string.local_high_score, 0),
+        text = stringResource(R.string.local_high_score, gameUiState.localHighScore),
         fontSize = 24.sp,
         fontWeight = FontWeight.Bold,
         modifier = Modifier.padding(8.dp)
@@ -67,16 +71,20 @@ fun WelcomeScores() {
 }
 
 @Composable
-fun WelcomeScreen() {
+fun WelcomeScreen(
+    viewModel: GameViewModel,
+    playGameClick: () -> Unit
+) {
+    val gameUiState by viewModel.uiState.collectAsState()
     Column(
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         WelcomeTitle()
         WelcomeButton(
             text = stringResource(R.string.play_game_button),
-            onClick = {}
+            onClick = playGameClick
         )
-        WelcomeScores()
+        WelcomeScores(gameUiState)
         WelcomeButton(
             text = stringResource(R.string.show_global_scores_button),
             onClick = {},
@@ -88,5 +96,7 @@ fun WelcomeScreen() {
 @Preview(showBackground = true)
 @Composable
 fun WelcomeScreenPreview() {
-    WelcomeScreen()
+    // Create a fake GameUiState for preview
+    val fakeUiState = GameUiState(lastScore = 5, localHighScore = 10)
+    WelcomeScores(gameUiState = fakeUiState)
 }
