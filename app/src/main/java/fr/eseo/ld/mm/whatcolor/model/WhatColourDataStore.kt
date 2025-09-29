6ce3846450d91c0e40404a46df2454df973273ad
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-09-29 10:52:17 +0200

    feature - whatColourDataStore to model the data to be persisted : the score

---
+++ app/src/main/java/fr/eseo/ld/mm/whatcolor/model/WhatColourDataStore.kt
@@ -0,0 +1,33 @@
+package fr.eseo.ld.mm.whatcolor.model
+
+import android.content.Context
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import fr.eseo.ld.mm.whatcolor.ui.state.GameUiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+private val Context.dataStore by preferencesDataStore(name = "what_colour")
+
+class WhatColourDataStore(private val context: Context) {
+    val uiStateFlow: Flow<GameUiState> = context.dataStore.data
+        .map { preferences ->
+            val lastScore = preferences[LOCAL_LAST_SCORE_KEY] ?: 0
+            val highScore = preferences[LOCAL_HIGH_SCORE_KEY] ?: 0
+            GameUiState(lastScore = lastScore, localHighScore = highScore)
+        }
+
+    suspend fun saveScores(lastScore: Int, highScore: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[LOCAL_LAST_SCORE_KEY] = lastScore
+            preferences[LOCAL_HIGH_SCORE_KEY] = highScore
+        }
+    }
+
+    companion object {
+        val LOCAL_LAST_SCORE_KEY = intPreferencesKey("local_last_int_key")
+        val LOCAL_HIGH_SCORE_KEY = intPreferencesKey("local_high_int_key")
+    }
+}
