d0c7fda4df9e8d00fc9d7d2b775474ccfb9f9d6c
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-09-29 08:36:24 +0200

    feature - box that display the name of color in another color

--- app/src/main/java/fr/eseo/ld/mm/whatcolor/ui/screens/GameScreenComposables.kt
+++ app/src/main/java/fr/eseo/ld/mm/whatcolor/ui/screens/GameScreenComposables.kt
@@ -1,6 +1,7 @@
 package fr.eseo.ld.mm.whatcolor.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,10 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fr.eseo.ld.mm.whatcolor.R
 import fr.eseo.ld.mm.whatcolor.model.PreviousGuess
 import kotlin.math.max
@@ -82,5 +88,28 @@ fun PreviousGuessList(
                 modifier = Modifier.padding(8.dp)
             )
         }
+    }
+}
+
+@Composable
+fun BoxWithText(
+    textId: Int,
+    colourId: Int,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .background(colorResource(id = R.color.colour_grey))
+            .padding(vertical = 16.dp)
+            .fillMaxWidth(0.75f)
+    ) {
+        Text(
+            text = stringResource(id = textId),
+            color = colorResource(id = colourId),
+            fontSize = 58.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
     }
 }
